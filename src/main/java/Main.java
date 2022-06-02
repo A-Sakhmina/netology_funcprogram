00dd5263a4cc775cfc30f088514aa0909bcdcbb7
Sakhmina <asadullaevasakhmina@gmail.com>
2022-06-03 00:01:03 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Function<String, List<String>> funcStringToVocab = (String str) -> {
+            return Arrays.stream(str.split(" "))
+                    .sorted()
+                    .collect(Collectors.toList());
+        };
+
+        //исходный текст сообщения местного жителя
+        String input = "кошка зашла на кухню";
+
+        //применяем написанную функцию к исходному тексту сообщения местного жителя
+        List<String> vocabularyList = funcStringToVocab.apply(input);
+        vocabularyList.forEach(System.out::println);
+
+    }
+
+}
